f2ac25e002c19f20c350fe795cb7b09f4a26f10f
Naman <namanbansal102@gmail.com>
2023-10-04 19:24:59 +0530

    Inital Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let playBut=document.querySelector('.play-song')
+console.log("Script for Spotify is Running");
+playBut.addEventListener('click',()=>{
+    console.log("Click On button is Running");
+    if () {
+        
+    }
+})
